1af60fc0970d7d124636161f7d06eec4bf647aa7
Saharsh Ananta Jaiswal <saharshcr7@gmail.com>
2021-03-04 16:19:46 +0530

    Added neew files

--- Stack/Largest Histogram Stack.cpp
+++ Stack/Largest Histogram Stack.cpp
@@ -1,27 +1,32 @@
-  class Solution {
-    public:
-        int largestRectangleArea(vector<int> &height) {
-            
-            int ret = 0;
-            height.push_back(0);
-            vector<int> index;
-            
-            for(int i = 0; i < height.size(); i++)
-            {
-                while(index.size() > 0 && height[index.back()] >= height[i])
-                {
-                    int h = height[index.back()];
-                    index.pop_back();
-                    
-                    int sidx = index.size() > 0 ? index.back() : -1;
-                    if(h * (i-sidx-1) > ret)
-                        ret = h * (i-sidx-1);
-                }
-                index.push_back(i);
-            }
-            
-            return ret;
-        }
-    };
 
-    //https://leetcode.com/problems/largest-rectangle-in-histogram/
+
+vector<vector<int>> getMergedIntervals(vector<vector<int>> intervals) {
+    
+    sort(intervals.begin(),intervals.end());
+    stack <pair<int,int>> s;
+    s.push(make_pair(intervals[0][0], intervals[0][1]));
+    for(int i=0;i<intervals.size();i++){
+       int start =  s.top().first;
+       int end =  s.top().second;
+        
+        if(end < intervals[i][0]){
+            s.push(make_pair(intervals[i][0], intervals[i][1]));
+        }
+        else if(end<intervals[i][1]){
+            end = intervals[i][1];
+            s.pop();
+            s.push(make_pair(start,end));
+        }
+    }
+    int count=0;
+    vector<vector<int>>results(s.size());
+    
+    while(!s.empty()){
+        results[count].push_back(s.top().first);
+        results[count].push_back(s.top().second);
+        count++;
+        s.pop();
+    }
+    sort(results.begin(),results.end());
+    return results;
+}
