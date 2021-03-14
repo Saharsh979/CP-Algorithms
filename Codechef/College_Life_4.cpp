c3fc6c53ee6bbe719188732f3d39e7c715a57dc7
Saharsh Ananta Jaiswal <saharshcr7@gmail.com>
2021-03-14 13:41:27 +0530

    ads

--- Codechef/College_Life_4.cpp
+++ Codechef/College_Life_4.cpp
@@ -9,145 +9,79 @@ int main()
     {
         long long int a, b, c, t, e, n, h;
         cin >> n >> e >> h >> a >> b >> c;
-
-        long long int possibleitems = 0;
-        if (e == h)
+        long long int ans = INT_MAX;
+        set<long long int> setvalues;
+        if ((e / 2) >= n)
         {
-            possibleitems = e;
+            long long int value = a * n;
+            ans = min(ans, value);
+            setvalues.insert(value);
         }
-        if (e > h)
+        if ((h / 3) >= n)
         {
-            possibleitems = h + (e - h) / 2;
+            long long int value = b * n;
+            ans = min(ans, value);
+            setvalues.insert(value);
         }
-        if (h > e)
+        if (min(e, h) >= n)
         {
-            possibleitems = e + (h - e) / 2;
+            long long int value = c * n;
+            ans = min(ans, value);
+            setvalues.insert(value);
         }
-        if (possibleitems < n)
+        if ((((h - n) / 2) >= 1) && ((h - n) / 2) >= n - e)
         {
-            cout << -1 << "\n";
-            continue;
-        }
-        else
-        {
-            set<long long int> setvalues;
-            if ((e / 2) >= n)
+            if (b < c)
             {
-                long long int value = a * n;
+                long long int temp = min(n - 1, (h - n) / 2);
+                long long int value = (b - c) * temp + n * c;
+                ans = min(ans, (b - c) * temp + n * c);
                 setvalues.insert(value);
-            }
-            if ((h / 3) >= n)
-            {
-                long long int value = b * n;
-                setvalues.insert(value);
-            }
-            if (min(e, h) >= n)
-            {
-                long long int value = c * n;
-                setvalues.insert(value);
-            }
-            if (((e / 2) >= 1 && (h / 3) >= 1 && ((e / 2) + (h / 3)) >= n))
-            {
-                long long int value;
-                if (a < b)
-                {
-                    value = min(n - 1, (e / 2)) * a + (n - min(n - 1, (e / 2))) * b;
-                }
-                else
-                {
-                    value = min(n - 1, (h / 3)) * b + (n - min(n - 1, (h / 3))) * a;
-                }
-                setvalues.insert(value);
-            }
-
-            if (((e - n) >= 1 && h >= 1 && (e + h) >= 2 * n))
-            {
-                long long int value;
-                if (a < c)
-                {
-                    value = min(n - 1, (e / 2)) * a + (n - min(n - 1, (e / 2))) * c;
-                }
-                else
-                {
-                    value = min(n - 1, min(h, e)) * c + (n - min(n - 1, min(h, e))) * a;
-                }
-                setvalues.insert(value);
-            }
-
-            if (((h - n) >= 2 && e >= 1 && (e + h) >= 2 * n))
-            {
-                long long int value;
-                if (b < c)
-                {
-                    value = min(n - 1, (h / 3)) * b + (n - min(n - 1, (h / 3))) * c;
-                }
-                else
-                {
-                    value = min(n - 1, min(h, e)) * c + (n - min(n - 1, min(h, e))) * b;
-                }
-                setvalues.insert(value);
-            }
-            if ((((h + e) / 2) >= (n - 2) && e >= n && h >= n + 1))
-            {
-                long long int value;
-                if (a == min(a, min(b, c)))
-                {
-                    long long int x1 = min(n - 2, e / 2) * a;
-                    long long int n1 = n - min(n - 2, e / 2);
-                    if (b < c)
-                    {
-                        value = min(n1 - 1, (h / 3)) * b + (n1 - min(n1 - 1, (e / 3))) * c + x1;
-                    }
-                    else
-                    {
-                        value = min(n1 - 1, min(h, e)) * c + (n1 - min(n1 - 1, min(h, e))) * b + x1;
-                    }
-                }
-                else if (b == min(a, min(b, c)))
-                {
-                    long long int x1 = min(n - 2, h / 3) * b;
-                    long long int n1 = n - min(n - 2, h / 3);
-                    if (a < c)
-                    {
-                        value = min(n1 - 1, (e / 2)) * a + (n1 - min(n1 - 1, (e / 2))) * c + x1;
-                    }
-                    else
-                    {
-                        value = min(n1 - 1, min(h, e)) * c + (n1 - min(n1 - 1, min(h, e))) * a + x1;
-                    }
-                }
-                else
-                {
-                    long long int x1 = min(n - 2, min(h, e)) * c;
-                    long long int n1 = n - min(n - 2, min(h, e));
-                    long long int e1 = e - min(n - 2, min(h, e));
-                    long long int h1 = h - min(n - 2, min(h, e));
-                    if (a < b)
-                    {
-                        value = min(n1 - 1, (e1 / 2)) * a + (n1 - min(n1 - 1, (e1 / 2))) * b + x1;
-                    }
-                    else
-                    {
-                        value = min(n1 - 1, (h1 / 3)) * b + (n1 - min(n1 - 1, (h1 / 3))) * a + x1;
-                    }
-                }
-                setvalues.insert(value);
-            }
-
-            if (setvalues.size() == 0)
-            {
-                cout << -1 << endl;
             }
             else
             {
-                long long int answer = INT_MAX;
-                for (auto it : setvalues)
-                {
-                    answer = min(answer, it);
-                }
-                cout << answer << endl;
+                long long int one = 1;
+                long long int temp = max(one, n - e);
+                long long int value = (b - c) * temp + n * c;
+                ans = min(ans, value);
+                setvalues.insert(value);
+            }
+        }
+        if (((e - n) >= 1) && ((e - n) >= n - h))
+        {
+            if (a < c)
+            {
+                long long int temp = min(n - 1, (e - n));
+                long long int value = (a - c) * temp + n * c;
+                ans = min(ans, value);
+                setvalues.insert(value);
+            }
+            else
+            {
+                long long int one = 1;
+                long long int temp = max(one, n - h);
+                long long int value = (a - c) * temp + n * c;
+                ans = min(ans, value);
+                setvalues.insert(value);
+            }
+        }
+        if ((((e / 2) >= 1) && (e/2 >= (3*n - h+2)/3))
+        {
+            if (a < b)
+            {
+                long long int temp = min(n - 1, e / 2);
+                long long int value = (a - b) * k + n * b;
+                ans = min(ans, value);
+                setvalues.insert(value);
+            }
+            else
+            {
+                long long int one = 1;
+                long long int temp = max(one, (3 * n - h + 2) / 3);
+                long long int value = (a - b) * k + n * b;
+                ans = min(ans, value);
+                setvalues.insert(value);
             }
         }
     }
-    return 0;
 }
